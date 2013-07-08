2aa959897619ef5d6cd50d2bd1b5faa831816018
Sol Tran <soltran@me.com>
2013-07-08 13:35:15 -0700

    Adjust production.rb. Added Figaro

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'jquery-ui-rails'
 
 gem 'jquery-fileupload-rails'
 
+gem 'figaro'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ InfiniteShirts::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => "infiniteprod",
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
