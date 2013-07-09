be8b00a8f34f378635f43e282f355006b52f9e55
Sol Tran <soltran@me.com>
2013-07-08 21:01:07 -0700

    Migrate wishes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708211022) do
+ActiveRecord::Schema.define(:version => 20130708224316) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -69,5 +69,12 @@ ActiveRecord::Schema.define(:version => 20130708211022) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "wishes", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
